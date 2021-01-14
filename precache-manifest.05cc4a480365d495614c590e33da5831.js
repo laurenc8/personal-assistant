5d6d08524c370b46da49709b2fda10a87d322aea
laurenc8 <laurenc8822@gmail.com>
2021-01-14 06:35:49 -0500

    Updates

---
+++ precache-manifest.05cc4a480365d495614c590e33da5831.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a80b9e00fbf300e3a4f5c4899e67383",
+    "url": "/personal-assistant/index.html"
+  },
+  {
+    "revision": "84c654e8f84fa28b40fe",
+    "url": "/personal-assistant/static/css/main.7c054c47.chunk.css"
+  },
+  {
+    "revision": "174471f639bf6a97ae7e",
+    "url": "/personal-assistant/static/js/2.5f60e662.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/personal-assistant/static/js/2.5f60e662.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84c654e8f84fa28b40fe",
+    "url": "/personal-assistant/static/js/main.20db84c2.chunk.js"
+  },
+  {
+    "revision": "6af68dc79e5ea81f8987",
+    "url": "/personal-assistant/static/js/runtime-main.9f1a1dcf.js"
+  }
+]);
